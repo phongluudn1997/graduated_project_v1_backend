5c2da1a0677121b565060ad13f266569eb1953c4
Huynh Phong Luu <luuhuynh@me.com>
2019-11-27 22:11:31 +0700

    disable get post by type

--- routes/post.js
+++ routes/post.js
@@ -15,7 +15,7 @@ var upload = multer({ storage: storage });
 
 router.post("/", checkToken, upload.single("image"), PostController.uploadPost);
 router.get("/", PostController.getPosts);
-router.get("/:type/", PostController.getPostsByType);
+// router.get("/:type/", PostController.getPostsByType);
 router.get("/:type/latest", PostController.getLatest);
 router.get("/:id", PostController.getPost);
 router.delete("/:_id", PostController.deleteById);
